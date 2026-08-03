a386401696801543c5ad9780c139dafbbcd72cf6
vilivonka <gromootvod@lenta.ru>
2026-08-03 16:59:28 +0300

    Add gh-pages deployment setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // или твой текущий плагин
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/battleCheess/', // 🌟 ВАЖНО: косые черты в начале и конце обязательны!
 })
